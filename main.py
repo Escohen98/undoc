5c03cda644c20e76c0c24ee240e4b00ef51850c3
Eric <escohen98@gmail.com>
2022-11-16 16:00:40 -0500

    Finished.

--- main.py
+++ main.py
@@ -1,54 +1,43 @@
 import os
-import shutil
 import zipfile
 import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 
+#Converts word document to text
 def main(file):
     filename = file[:file.rfind(".")]
     extension = file[file.rfind(".")+1:]
     #Checks to make sure correct file path
-    if (not (extension == "docx" or extension == "doc")):
-        print("Invalid file. Please choose a 'doc' or 'docx'")
+    if (not ((extension == "docx" or extension == "doc") and os.path.isfile(file))):
+        print("Invalid file. Please choose an existing 'doc' or 'docx' file.")
         main(input("file: "))
         exit()
 
-    #Convert a document to zip
-    folder = filename #Custom folder name
-    newfile = docToZip(folder)
-
-    # Rename the file
-    shutil.copyfile(file, newfile)
-
-    #Extracting files to folder
-    with zipfile.ZipFile(newfile, 'r') as zip_ref:
-        zip_ref.extractall(folder)
-
-    zipToTxt(folder)
-
-    os.remove(newfile) #Deletes zip
-    shutil.rmtree(folder) #Deletes extracted directory tree
+    docToTxt(filename)
     print("Done.")
 
-#Converts the zip xml content to text
-def zipToTxt(folder):
+#Creates zip object from word document
+#Pulls and formats xml
+#Extracts text from XML and parses to text document.
+def docToTxt(folder):
     #Reading lines. Efficient?
     doc = zipfile.ZipFile(folder + ".docx")
-    prettyXML = BeautifulSoup(doc.read('word/document.xml'), features="xml").prettify() #Turns 1 line into formatted XML
+    #Turns 1 line into formatted XML
+    prettyXML = BeautifulSoup(doc.read('word/document.xml'), features="xml").prettify()
     root = ET.fromstring(prettyXML)
 
     namespace = {'w': "http://schemas.openxmlformats.org/wordprocessingml/2006/main"}
     text = root.findall('.//w:t', namespace) #All text is stored in <w:t*>
-    g = open(folder + ".txt", 'w')
 
-    print("hi")
+    g = open(overwrite(folder), 'w')
     for t in text: #XML is one line, but just to be safe..
-        print(t.text)
         g.write(t.text)
     g.close()
 
-def docToZip(folder):
-    newfile =  folder + ".zip"
+#Checks if output file exists
+#Asks if overwrite
+def overwrite(folder):
+    newfile =  folder + ".txt"
     if(os.path.isfile(newfile)):
         overwrite = False
         while(True):
@@ -60,27 +49,12 @@ def docToZip(folder):
                 break
             print("Please answer yes or no")
 
+        #Uses windows naming convention of (n)
         if(not overwrite):
             x = 0
             while(os.path.isfile(newfile)):
                 x+=1
-                folder = f"{filename}({x})"
-                newfile = folder + ".zip"
+                newfile = f"{folder}({x}).txt"
     return newfile
-
-def xmlToTxt(folder):
-
-    tree = ET.parse(f"./{folder}/word/document.xml")
-    root = tree.getroot()
-    myroot = ET.fromstring(f"./{folder}/word/document.xml")
-
-    g = open(folder + ".txt", 'w')
-
-    #for x in root.findall('field'):
-    for x in myroot[0]:
-        print(x.text)
-        g.write(x.text)
-
-    g.close()
 
 main(input("file: "))
